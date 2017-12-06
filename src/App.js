2d625b914574d204444bc4d732b9a2a018db1b91
Fredrik Mäkilä <fredrik.makila@gmail.com>
2017-12-06 01:25:41 +0000

    Remove unused import

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import RegisterPageContainer from './containers/RegisterPage';
-import { Route, Redirect, BrowserRouter, Switch } from 'react-router-dom';
+import { Route, Redirect, Switch } from 'react-router-dom';
 import Header from './containers/Header';
 import Footer from './containers/Footer';
 import PrivateRoute from './containers/PrivateRoute';
@@ -10,35 +10,32 @@ import Home from './containers/Home';
 import Dashboard from './containers/Dashboard';
 
 class App extends Component {
+  render() {
+    return (
+      <div className={'box'}>
+        <Header />
+        <section className={'row content'}>
+          <Switch>
+            <Route exact path={'/'} component={Home} />
+            <Route exact path={'/login'} component={Login} />
 
-    render() {
-        return (
-                <div className={'box'}>
-                    <Header />
-                    <section className={'row content'}>
+            <PrivateRoute exact path={'/dashboard'} component={Dashboard} />
+            <PrivateRoute
+              exact
+              path={'/register'}
+              component={RegisterPageContainer}
+            />
 
-                        <Switch>
-                            <Route exact path={'/'} component={Home}/>
-                            <Route exact path={'/login'} component={Login}/>
-
-                            <PrivateRoute exact path={'/dashboard'} component={Dashboard}/>
-                            <PrivateRoute exact path={'/register'} component={RegisterPageContainer}/>
-
-                            <Route exact path={'/404'} component={NotFound}/>
-                            <Redirect from='*' to='/404' />
-                        </Switch>
-
-                    </section>
-                    <Footer />
-                </div>
-        );
-    }
+            <Route exact path={'/404'} component={NotFound} />
+            <Redirect from="*" to="/404" />
+          </Switch>
+        </section>
+        <Footer />
+      </div>
+    );
+  }
 }
 
-
-const NotFound = () => (
-    <div>Where are you going?</div>
-);
-
+const NotFound = () => <div>Where are you going?</div>;
 
 export default App;
