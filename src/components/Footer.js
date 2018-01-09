dd79a1534a71d7d3e6e5a5793a48de4308539487
Fredrik Mäkilä <fredrik.makila@gmail.com>
2018-01-09 10:48:50 +0000

    Change style of Header and Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import './Footer.css';
 
 const Footer = () => {
-  return <footer className={'row footer'} />;
+  return <footer className={'Footer'} />;
 };
 
 export default Footer;
