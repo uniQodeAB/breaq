900e4224175b8d85589c09d8ee56524d0a9bbe2d
Fredrik Mäkilä <fredrik.makila@gmail.com>
2018-01-09 09:31:49 +0000

    Remove debug code

--- src/containers/MapContainer.js
+++ src/containers/MapContainer.js
@@ -3,10 +3,6 @@ import { connect } from 'react-redux';
 import { compose } from 'redux';
 import Map from '../components/Map';
 
-const test = () => {
-  console.log('this is a test');
-};
-
 export default compose(
   firebaseConnect((props, store) => {
     const { auth } = store.getState().firebase;
@@ -17,7 +13,6 @@ export default compose(
     user: data.users,
     base: data.users && data.users[auth.uid] && data.users[auth.uid].base,
     employees:
-      data.users && data.users[auth.uid] && data.users[auth.uid].employees,
-    test: test
+      data.users && data.users[auth.uid] && data.users[auth.uid].employees
   }))
 )(Map);
