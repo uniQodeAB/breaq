52cfe1c276948afbbb27230973c930176179e5ca
Fredrik Mäkilä <fredrik.makila@gmail.com>
2018-01-13 20:23:41 +0000

    Update App test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,39 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
+import Enzyme, { mount, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import HeaderContainer from './containers/HeaderContainer';
+import Footer from './components/Footer';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+describe('App', () => {
+  let app;
+
+  beforeEach(() => {
+    Enzyme.configure({ adapter: new Adapter() });
+    app = shallow(<App />);
+  });
+
+  it('should always render a div', () => {
+    const divs = app.find('div');
+
+    expect(divs.length).toBeGreaterThan(0);
+  });
+
+  describe('the rendered div', () => {
+    it('contains everyting else that gets rendered', () => {
+      const divs = app.find('div');
+
+      const wrappingDiv = divs.first();
+
+      expect(wrappingDiv.children()).toEqual(shallow(<App />).children());
+    });
+
+    it('always renders a `HeaderContainer`', () => {
+      expect(app.contains(<HeaderContainer />)).toBe(true);
+    });
+
+    it('always renders a `Footer`', () => {
+      expect(app.contains(<Footer />)).toBe(true);
+    });
+  });
 });
