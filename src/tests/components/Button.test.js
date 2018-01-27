2a251648d835a85b2954ef9db6d6630454468e14
Fredrik Mäkilä <fredrik.makila@gmail.com>
2018-01-27 09:26:46 +0000

    Add test for Button component

---
+++ src/tests/components/Button.test.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import configureEnzyme from '../testUtil.test';
+import Button from '../../components/Button';
+
+configureEnzyme();
+
+describe('Button', () => {
+  let button;
+  let mockOnClick;
+
+  beforeEach(() => {
+    mockOnClick = jest.fn();
+    button = shallow(<Button onClick={mockOnClick}>text</Button>);
+  });
+
+  it('should render a `button`', () => {
+    expect(button.find('button').length).toBe(1);
+  });
+
+  it('should call onClick prop on button click', () => {
+    button.simulate('click');
+
+    expect(mockOnClick.mock.calls.length).toBe(1);
+  });
+
+  it('shoudl render children as content', () => {
+    expect(button.text()).toBe('text');
+  });
+});
