e4bdaa642cc0d36c09b1425df3b5c5d6aa996176
Fredrik Mäkilä <fredrik.makila@gmail.com>
2018-03-18 10:22:17 +0000

    Remove API keys

--- src/firebase.js
+++ src/firebase.js
@@ -3,11 +3,11 @@ import 'firebase/firestore';
 // Initialize Firebase
 const config = {
   apiKey: `${process.env.REACT_APP_FIREBASE_API_KEY}`,
-  authDomain: 'breaq-uniqode.firebaseapp.com',
-  databaseURL: 'https://breaq-uniqode.firebaseio.com',
-  projectId: 'breaq-uniqode',
-  storageBucket: 'breaq-uniqode.appspot.com',
-  messagingSenderId: '728259108467'
+  authDomain: `${process.env.REACT_APP_FIREBASE_AUTH_DOMAIN}`,
+  databaseURL: `${process.env.REACT_APP_FIREBASE_DATABASE_URL}`,
+  projectId: `${process.env.REACT_APP_FIREBASE_PROJECT_ID}`,
+  storageBucket: `${process.env.REACT_APP_FIREBASE_STORAGE_BUCKET}`,
+  messagingSenderId: `${process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID}`
 };
 firebase.initializeApp(config);
 
