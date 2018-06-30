fef6d33b70db15ca0bfac3398bcecd013e95f3b7
Fredrik Mäkilä <fredrik.makila@gmail.com>
2018-06-30 20:13:46 +0100

    #158307199 Add Redux boilerplate

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,24 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from './App';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { EnthusiasmAction } from './actions/index';
+import Hello from './containers/Hello';
+import { enthusiasm } from './reducers/index';
+import { IStoreState } from './types/index';
+
+const store = createStore<IStoreState, EnthusiasmAction, any, any>(enthusiasm, {
+  enthusiasmLevel: 1,
+  languageName: 'TypeScript',
+});
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <Hello />
+  </Provider>,
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
