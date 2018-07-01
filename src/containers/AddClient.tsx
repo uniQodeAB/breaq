709ee54d80c3992eee192ae7a8219748d88fa580
Fredrik Mäkilä <fredrik.makila@gmail.com>
2018-07-01 19:54:12 +0100

    #158307199 add action tests

--- src/containers/AddClient.tsx
+++ src/containers/AddClient.tsx
@@ -2,11 +2,13 @@ import { connect } from 'react-redux';
 import { firestoreConnect } from 'react-redux-firebase';
 import { compose } from 'redux';
 
-import { submitClient } from '../actions/firebaseActions';
+import { ThunkDispatch } from 'redux-thunk';
+import { ClientAction } from '../actions';
+import { submitClient } from '../actions/appActions';
 import AddClient from '../components/AddClient';
 import { IClient, IStoreState } from '../types';
 
-function mapDispatchToProps(dispatch:any) {
+function mapDispatchToProps(dispatch:ThunkDispatch<IStoreState, void, ClientAction>) {
   return {
     submitClient: (client:IClient) => dispatch(submitClient(client)),
   };
